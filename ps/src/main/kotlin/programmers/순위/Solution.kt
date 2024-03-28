8860e8d1fa6e52cf3fc068d2e2b8319178a11979
devjun10 <devjun1023@gmail.com>
2024-03-28 20:47:14 +0900

    ps: 순위 - 플로이드 와샬

---
+++ ps/src/main/kotlin/programmers/순위/Solution.kt
@@ -0,0 +1,45 @@
+package programmers.순위
+
+class Solution {
+    fun solution(
+        n: Int,
+        results: Array<IntArray>
+    ): Int {
+
+        val win = Array(n + 1) { BooleanArray(n + 1) }
+        val lose = Array(n + 1) { BooleanArray(n + 1) }
+
+        for (result in results) {
+            val winner = result[0]
+            val loser = result[1]
+            win[winner][loser] = true
+            lose[loser][winner] = true
+        }
+        for (x in 1..n) {
+            for (y in 1..n) {
+                for (z in 1..n) {
+                    if (win[y][x] && win[x][z]) {
+                        win[y][z] = true
+                    }
+                    if (lose[y][x] && lose[x][z]) {
+                        lose[y][z] = true
+                    }
+                }
+            }
+        }
+
+        var answer = 0
+        for (x in 1..n) {
+            var count = 0
+            for (y in 1..n) {
+                if (win[x][y] || lose[x][y]) {
+                    count++
+                }
+            }
+            if (count == n - 1) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
