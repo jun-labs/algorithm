0d6b34dbd57bfb460f1bc36c1090d2706db22500
devjun10 <devjun1023@gmail.com>
2024-04-15 05:36:07 +0900

    ps: 후보 추천하기 - 구현, 정렬

---
+++ ps/src/main/kotlin/boj/boj_1713/Main.kt
@@ -0,0 +1,35 @@
+package boj.boj_1713
+
+fun main() {
+    val n = readln().toInt()
+    val totalCount = readln().toInt()
+    val students = readln().split(" ")
+        .map { it.toInt() }
+
+    val frames = mutableListOf<Candidate>()
+    var time = 0
+
+    for (student in students) {
+        val existing = frames.find { it.student == student }
+        if (existing != null) {
+            existing.votes += 1
+        } else {
+            if (frames.size < n) {
+                frames.add(Candidate(student, 1, time++))
+            } else {
+                frames.sortWith(compareBy({ it.votes }, { it.time }))
+                frames.removeAt(0)
+                frames.add(Candidate(student, 1, time++))
+            }
+        }
+    }
+
+    val result = frames.map { it.student }.sorted()
+    println(result.joinToString(" "))
+}
+
+data class Candidate(
+    val student: Int,
+    var votes: Int,
+    var time: Int,
+)
