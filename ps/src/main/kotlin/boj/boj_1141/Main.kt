379bdccdd649344f729bffd188ecc532e723e764
devjun10 <devjun1023@gmail.com>
2024-04-25 21:26:07 +0900

    ps: 접두사 - 정렬

---
+++ ps/src/main/kotlin/boj/boj_1141/Main.kt
@@ -0,0 +1,24 @@
+package boj.boj_1141
+
+fun main() {
+    val n = readln().toInt()
+    val words = mutableListOf<String>()
+    for (input in 0 until n) {
+        words.add(readln())
+    }
+
+    words.sortWith(compareBy<String> { it.length }.thenBy { it })
+
+    val result = mutableListOf<String>()
+
+    outer@ for (i in 0 until words.size) {
+        for (j in i + 1 until words.size) {
+            if (words[j].startsWith(words[i])) {
+                continue@outer
+            }
+        }
+        result.add(words[i])
+    }
+
+    println(result.size)
+}
