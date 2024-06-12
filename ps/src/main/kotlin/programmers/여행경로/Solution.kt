857fe6480132690784058efcb553dc901ced9036
devjun10 <devjun1023@gmail.com>
2024-06-12 23:09:22 +0900

    ps: [RE] 여행경로 - 2024.06.12 - DFS

--- ps/src/main/kotlin/programmers/여행경로/Solution.kt
+++ ps/src/main/kotlin/programmers/여행경로/Solution.kt
@@ -1,42 +1,52 @@
 package programmers.여행경로
 
 class Solution {
-    companion object {
-        private const val START = 0
-        private const val NEXT = 1
-    }
-
-    private var totalCityCount = 0
-    private var tickets: Array<Array<String>>? = null
-    private var visited: Array<Boolean>? = null
+    private lateinit var visited: BooleanArray
+    private lateinit var tickets: Array<Array<String>>
     private val allRoutes = mutableListOf<String>()
 
     fun solution(tickets: Array<Array<String>>): Array<String> {
-        totalCityCount = tickets.size
         this.tickets = tickets
-        visited = Array(totalCityCount) { false }
-        dfs("ICN", "ICN", 0)
+        this.visited = BooleanArray(tickets.size) { false }
+
+        dfs("ICN", mutableListOf("ICN"))
         allRoutes.sort()
-        return allRoutes[0].split(" ")
+        return allRoutes[0].split(", ")
             .toTypedArray()
     }
 
     private fun dfs(
         start: String,
-        route: String,
-        count: Int
+        routes: MutableList<String>,
     ) {
-        if (count == totalCityCount) {
-            allRoutes.add(route)
+        if (routes.size == tickets.size + 1) {
+            allRoutes.add(routes.joinToString(", "))
             return
         }
-        for (idx in 0 until totalCityCount) {
-            if (start == tickets!![idx][START] && !visited!![idx]) {
-                visited!![idx] = true
-                val next = tickets!![idx][NEXT]
-                dfs(next, "$route $next", count + 1)
-                visited!![idx] = false
+
+        for (idx in tickets.indices) {
+            if (start == tickets[idx][0] && !visited[idx]) {
+                visited[idx] = true
+                val next = tickets[idx][1]
+                routes.add(next)
+                dfs(next, routes)
+                routes.removeAt(routes.size - 1)
+                visited[idx] = false
             }
         }
     }
+}
+
+fun main() {
+    val tickets: Array<Array<String>> = arrayOf(
+        arrayOf("ICN", "SFO"),
+        arrayOf("ICN", "ATL"),
+        arrayOf("SFO", "ATL"),
+        arrayOf("ATL", "ICN"),
+        arrayOf("ATL", "SFO")
+    )
+
+    val solution = Solution()
+    val result = solution.solution(tickets)
+    println(result.joinToString(", "))
 }
