c2afcad9011e05f6e82169fac867f44b171ef523
devjun10 <devjun1023@gmail.com>
2024-06-12 10:53:17 +0900

    ps: [RE] 스위치 켜고 끄기 - 2024.06.12 - 구현

--- ps/src/main/kotlin/boj/boj_1244/Main.kt
+++ ps/src/main/kotlin/boj/boj_1244/Main.kt
@@ -6,19 +6,25 @@ fun main() {
         .map { it.toInt() }
         .toIntArray()
 
-    val students = readln().toInt()
-    repeat(students) {
+    val studentCount = readln().toInt()
+
+    repeat(studentCount) {
         val (gender, number) = readln().split(" ")
             .map { it.toInt() }
 
         if (gender == 1) {
-            for (i in number - 1 until n step number) {
-                bulbs[i] = bulbs[i].xor(1)
+            for (idx in bulbs.indices) {
+                if ((idx + 1) % number == 0) {
+                    bulbs[idx] = bulbs[idx].xor(1)
+                }
             }
-        } else if (gender == 2) {
+        } else {
             var left = number - 1
             var right = number - 1
-            while (left > 0 && right < n - 1 && bulbs[left - 1] == bulbs[right + 1]) {
+            while (left > 0 && right < n - 1) {
+                if (bulbs[left - 1] != bulbs[right + 1]) {
+                    break
+                }
                 left--
                 right++
             }
@@ -28,8 +34,11 @@ fun main() {
         }
     }
 
-    bulbs.forEachIndexed { index, state ->
-        print("$state ")
-        if ((index + 1) % 20 == 0) println()
+    for (idx in bulbs.indices) {
+        val bulb = bulbs[idx]
+        print("$bulb ")
+        if ((idx + 1) % 20 == 0) {
+            println()
+        }
     }
 }
