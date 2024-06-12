b109997b37388596950250b35433a795be74319f
devjun10 <devjun1023@gmail.com>
2024-06-13 01:47:41 +0900

    ps: [RE] 단어 변환 - 2024.06.13 - DFS

--- ps/src/main/kotlin/programmers/단어_변환/kotlin/Solution.kt
+++ ps/src/main/kotlin/programmers/단어_변환/kotlin/Solution.kt
@@ -1,16 +1,31 @@
-package programmers.단어_변환
+package programmers.단어_변환.kotlin
 
-import java.util.*
+import java.util.LinkedList
+import kotlin.math.min
 
 class Solution {
     private lateinit var visited: Array<Boolean>
     private val queue = LinkedList<Word>()
+    private lateinit var target: String
+    private lateinit var words: Array<String>
+    private var answer = 100_000
 
     fun solution(
         begin: String,
         target: String,
-        words: Array<String>
+        words: Array<String>,
     ): Int {
+        if (!words.contains(target)) {
+            return 0
+        }
+        this.target = target
+        this.words = words
+        visited = Array(words.size) { false }
+        dfs(begin, 0)
+        return answer
+    }
+
+    private fun bfs(begin: String, ): Int {
         if (!words.contains(target)) {
             return 0
         }
@@ -36,9 +51,43 @@ class Solution {
         return answer
     }
 
+    private fun dfs(
+        word: String,
+        count: Int,
+    ) {
+        if (word == target) {
+            answer = min(answer, count)
+            return
+        }
+
+        for (idx in words.indices) {
+            if (!visited[idx] && isDifferentOnlyOneCharacter(word, words[idx])) {
+                visited[idx] = true
+                dfs(words[idx], count + 1)
+                visited[idx] = false
+            }
+        }
+    }
+
+    private fun isDifferentOnlyOneCharacter(
+        wordA: String,
+        wordB: String,
+    ): Boolean {
+        var count = 0
+        for (idx in wordA.indices) {
+            if (wordA[idx] != wordB[idx]) {
+                count++
+            }
+            if (count > 1) {
+                return false
+            }
+        }
+        return count == 1
+    }
+
     private fun isDifferentOnlyOneCharacter(
         wordA: Word,
-        wordB: String
+        wordB: String,
     ): Boolean {
         if (!wordA.hasSameLength(wordB)) {
             return false
@@ -58,7 +107,7 @@ class Solution {
 
 data class Word(
     val value: String,
-    val count: Int
+    val count: Int,
 ) {
 
     val length: Int
