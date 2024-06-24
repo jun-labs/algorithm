c8eefdc31b8829eab5d34d772fd66512684a1f0b
devjun10 <devjun1023@gmail.com>
2024-06-24 21:13:10 +0900

    ps: 징검다리 - 2024.06.24 - DP

---
+++ ps/src/main/kotlin/softeer/징검다리/Main.java
@@ -0,0 +1,32 @@
+package softeer.징검다리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] numbers = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] < numbers[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int count : dp) {
+            answer = Math.max(answer, count);
+        }
+        System.out.println(answer);
+    }
+}
