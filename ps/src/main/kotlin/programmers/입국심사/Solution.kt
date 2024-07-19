c8b6bce04a841c9e6fe0a886d6ff650e83f7974e
devjun10 <devjun1023@gmail.com>
2024-07-19 18:37:09 +0900

    ps: [RE] 입국심사 - 이분탐색

--- ps/src/main/kotlin/programmers/입국심사/Solution.kt
+++ ps/src/main/kotlin/programmers/입국심사/Solution.kt
@@ -3,22 +3,18 @@ package programmers.입국심사
 class Solution {
     fun solution(
         n: Int,
-        times: IntArray
+        times: IntArray,
     ): Long {
         times.sort()
-        var answer: Long = 0
-
-        var start: Long = 0
-        var end: Long = n * times.last().toLong()
+        var answer = 0L
+        var start = 0L
+        var end = n * times.last().toLong()
         var mid: Long
-
         while (start <= end) {
             mid = (start + end) / 2
-
             val totalCount = times.fold(0L) { count, time ->
                 count + (mid / time)
             }
-
             if (totalCount < n) {
                 start = mid + 1
             } else {
