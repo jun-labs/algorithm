efb21a84d39117996ef84067937442cef51a0841
devjun10 <>
2024-10-03 17:06:35 +0900

    ps: [RE] 올바른 괄호 - 2024.10.03 - 스택

--- ps/src/main/kotlin/programmers/올바른_괄호/Solution.java
+++ ps/src/main/kotlin/programmers/올바른_괄호/Solution.java
@@ -2,32 +2,26 @@ package programmers.올바른_괄호;
 
 import java.util.Stack;
 
-class Solution {
+public class Solution {
 
     private final Stack<Character> stack = new Stack<>();
 
-    boolean solution(String s) {
+    public boolean solution(String s) {
         if (s.length() % 2 != 0) {
             return false;
         }
-        if (s.startsWith(")")) {
-            return false;
-        }
         for (int index = 0; index < s.length(); index++) {
-            char character = s.charAt(index);
-            if (stack.isEmpty()) {
-                stack.push(character);
-            } else if (character == '(') {
-                stack.push(character);
-            } else if (character == ')') {
-                char peek = stack.peek();
-                if (peek == '(') {
-                    stack.pop();
-                } else {
-                    return false;
-                }
+            char bracket = s.charAt(index);
+            if (bracket == '(') {
+                stack.push(bracket);
             } else {
-                stack.push(character);
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(bracket);
+                }
             }
         }
         return stack.isEmpty();
